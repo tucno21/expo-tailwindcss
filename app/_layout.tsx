ea860462b73179aaedbdb4f95d1f70044b65b03a
tucno21 <carlitostucno@gmail.com>
2025-08-04 18:49:00 -0500

    add tailwind css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
+import "./../global.css";
 
 export default function RootLayout() {
 
--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from 'react-native';
 
 const index = () => {
     return (
-        <View>
-            <Text>index</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
         </View>
     );
 };
