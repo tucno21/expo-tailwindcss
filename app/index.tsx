b5318384d2f9e5fbbe10e320e7b182cd1e1b3161
tucno21 <carlitostucno@gmail.com>
2025-08-04 16:23:40 -0500

    limpieza base

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    );
+};
+
+export default index;
